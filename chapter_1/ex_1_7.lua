5037b9d6f37b9b4aa0d13e2ba1912ca283920e52
Rick Lawson <lawson89@gmail.com>
2023-10-19 10:16:52 -0400

    more learning

--- chapter_1/ex_1_7.lua
+++ chapter_1/ex_1_7.lua
@@ -1,15 +1,6 @@
--- ex 1.6
-x = false
-function is_boolean(val)
-    if val == false or val == true then
-      return true
-    else
-      return false
-    end
-end
-
-print(is_boolean(true))
-print(is_boolean('x'))
-print(is_boolean(nil))
-print(is_boolean(false))
-print(is_boolean(5))
+-- ex 1.7
+x = true
+y = true
+z = true
+print(x and y and (not z))
+print(x and y and not z)
--- chapter_1/hello_world.lua
+++ chapter_1/hello_world.lua
@@ -1,1 +1,37 @@
 print("hello lua world!")
+a = {}
+a[1] = "x"
+a[2] = "x"
+a[3] = "x"
+print(#a)
+a[2]=nil
+print(#a)
+a[4]="w"
+print(#a)
+a[2]=nil
+print(#a)
+a[4]=nil
+print(#a)
+--   > a = {}
+--    > a[1] = "x"
+--    > a[2] = "y"
+--    > a[3] = "z"
+--    > #a
+--    3   -- okay
+--    > a[2] = nil
+--    > #a
+--    1   -- wat
+--    > a[4] = "w"
+--    > #a
+--    4   -- wat
+--    > a[2] = nil
+--    > #a
+--    4   -- okay (I guess)
+--    > a[4] = nil
+--    > #a
+--    1   -- wat
+a = {'x','y','z'}
+print(#a)
+for x,y in ipairs(a) do
+  print(x);print(y)
+end
